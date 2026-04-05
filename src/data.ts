849f5b9f9c149271d57e6560491288c00e018440
Ehsan <e24saeedi@gmail.com>
2026-04-04 23:36:35 -0700

    Update teaching experience

--- src/data.ts
+++ src/data.ts
@@ -180,22 +180,29 @@ export const workExperience: WorkExperience[] = [
 
 export const teachingExperience: TeachingExperience[] = [
   {
+    course: "Database Management Systems",
+    period: "Spring-2026",
+    role: "Graduate Teaching Assistant",
+    institution: "University Of Nevada-Reno",
+    responsibilities: "Grading, Mentorship, Practice sessions"
+  },
+  {
     course: "Cyber AI",
-    period: "Fall-2023",
+    period: "Fall-2024, Fall-2023",
     role: "Graduate Teaching Assistant",
     institution: "University Of Nevada-Reno",
     responsibilities: "Grading, Mentorship, Practice sessions"
   },
   {
     course: "Design Patterns",
-    period: "Fall-2023, Fall-2021",
+    period: "Fall-2025, Fall-2024, Fall-2023, Fall-2021",
     role: "Graduate Teaching Assistant",
     institution: "University Of Nevada-Reno",
     responsibilities: "Grading, Mentorship, Practice sessions"
   },
   {
     course: "Testing and DevOps",
-    period: "Fall-2023",
+    period: "Spring-2025, Spring-2024, Fall-2023",
     role: "Graduate Teaching Assistant",
     institution: "University Of Nevada-Reno",
     responsibilities: "Grading, Mentorship, Practice sessions"
