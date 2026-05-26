52a2455520b520c0a6a30160f7ea3e2ec0e5f69d
ehsan saeedi <e24saeedi@gmail.com>
2026-05-26 10:56:24 -0700

    Update services

--- src/data/services.ts
+++ src/data/services.ts
@@ -4,7 +4,7 @@ export const services: Service[] = [
   { title: "Reviewer", description: "Nevada Undergraduate Research Award (NURA), 2024-2026." },
   { title: "Technical Judge", description: "Nevada SkillsUSA (NVSkills) Computer Programming Competition, 2026." },{ title: "Conference Reviewer", description: "ICECET 2025, CCNC 2024, CCGRID 2023, IEEE CLOUD 2023." },
   { title: "Journal Reviewer", description: "ACM Computing Surveys, IEEE Transactions on Parallel and Distributed Systems, IEEE Access, Journal of Supercomputing, Journal of Computing, Journal of Grid Computing, Cluster Computing, Internet of Things and Cloud Computing, Discover Computing, Imaging Science Journal." },
-  { title: "Technical Program Committee (TPC) Member", description: "CSAE 2025, ICACTE 2025." },
+  { title: "Technical Program Committee (TPC) Member", description: "CSAE 2025, ICACTE 2025, ICACTE 2026." },
   { title: "Executive Member", description: "25th International Computer Conference (CSICC 2020)." },
   { title: "Executive Member", description: "IUST Cloud Computing Center (2018-2021)." },
   { title: "Tester", description: "Natural Language Processing Laboratory (2017)." },
